a62e82412a4ee7283e5f45d9c5a62023e1ad8888
kouga99 <167828897+kouga99@users.noreply.github.com>
2024-09-15 19:41:28 -0300

    cadastrar metas

--- index.js
+++ index.js
@@ -10,9 +10,24 @@
 
 const { select, input } = require ('@inquirer/prompts');
 
-const cadastrarMeta = async () =>{
-    const meta = await 
+let meta = {
+    value: 'Tomar 3L de água por dia',
+    checked: false,
 }
+let metas = [meta];
+
+const cadastrarMeta = async () =>{
+    const meta = await input ({message:"Digite a meta:"})
+
+    if(meta.length == 0){
+        console.log('A meta não pode ser vazia')
+        return
+    }
+
+    metas.push(
+        {value:meta, checked:false}
+    )
+};
 
 const start = async () => {
     while(true){
@@ -38,6 +53,7 @@ const start = async () => {
         switch(opcao) {
             case "cadastrar":
                 await cadastrarMeta()
+                console.log(metas)
                 break
             case "listar":
                 console.log("vamos listar")
