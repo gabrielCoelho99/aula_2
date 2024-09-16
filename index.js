5c208d899bc5e498f94eb04675275ef2dc5dabb8
kouga99 <167828897+kouga99@users.noreply.github.com>
2024-09-16 13:38:05 -0300

    metas marcadas/desmarcadas

--- index.js
+++ index.js
@@ -36,14 +36,16 @@ const listarMetas = async () =>{
         instructions: false
     })
 
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
     if(respostas.length==0){
         console.log("Nenhuma meta selecionada!")
         return
     }
 
-    metas.forEach((m) => {
-        m.checked = false
-    })
+    
 
     respostas.forEach((resposta) => {
         const meta = metas.find((m)=>{
