1c5d36cd69800cb2b7bf4d6b7edcdf220d8ff1e6
kouga99 <167828897+kouga99@users.noreply.github.com>
2024-09-16 13:06:11 -0300

    listar metas

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 
 // start();
 
-const { select, input } = require ('@inquirer/prompts');
+const { select, input, checkbox } = require ('@inquirer/prompts');
 
 let meta = {
     value: 'Tomar 3L de água por dia',
@@ -27,6 +27,33 @@ const cadastrarMeta = async () =>{
     metas.push(
         {value:meta, checked:false}
     )
+};
+
+const listarMetas = async () =>{
+    const respostas = await checkbox({
+        message:"Use as setas para mudar de meta, o espaço para marcar ou desmarcar ou o enter para finalizar essa etapa",
+        choices:[...metas],
+        instructions: false
+    })
+
+    if(respostas.length==0){
+        console.log("Nenhuma meta selecionada!")
+        return
+    }
+
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
+    respostas.forEach((resposta) => {
+        const meta = metas.find((m)=>{
+            return m.value == resposta
+        })
+
+        meta.checked = true
+    })
+
+    console.log('Meta(s) marcadas como concluída(s)')
 };
 
 const start = async () => {
@@ -56,7 +83,7 @@ const start = async () => {
                 console.log(metas)
                 break
             case "listar":
-                console.log("vamos listar")
+                await listarMetas()
                 break
             case "sair":
                 console.log("Até a próxima")
