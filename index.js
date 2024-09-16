5f56db56f3f13cc42bbe9b5998d564967883e22c
kouga99 <167828897+kouga99@users.noreply.github.com>
2024-09-16 18:58:50 -0300

    metas abertas

--- index.js
+++ index.js
@@ -69,9 +69,26 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message:"Metas Realizadas",
+        message:"Metas Realizadas " + realizadas.length,
         choices:[...realizadas]
     })
+};
+
+const metasAbertas = async ()=>{
+    const abertas = metas.filter((meta)=> {
+        return meta.checked != true
+    })
+
+    if (abertas.length == 0) {
+        console.log("Não existem metas abertas! =)")
+        return
+    }
+
+    await select ({
+        message:"Metas Abertas " + abertas.length,
+        choices: [...abertas]
+    })
+    
 };
 
 const start = async () => {
@@ -92,6 +109,10 @@ const start = async () => {
                     value:"realizadas"
                 },
                 {
+                    name:"Metas Abertas",
+                    value:"abertas"
+                },
+                {
                     name:"sair",
                     value:"sair"
                 }
@@ -109,6 +130,9 @@ const start = async () => {
                 break
             case "realizadas":
                 await metasRealizadas()
+                break
+            case "abertas":
+                await metasAbertas()
                 break
             case "sair":
                 console.log("Até a próxima meu chapa!")
